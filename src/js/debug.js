00473f97513485d4113fcf56b339c05cdbfe82a1
James Atkin <04jatkin@gmail.com>
2013-06-11 22:23:48 +0100

    Removed event proxy code, because I'm handling it a different way. Beginning to solve issue #12

--- src/js/debug.js
+++ src/js/debug.js
@@ -1,24 +1,15 @@
 var puckt = puckt || {};
 
 puckt.debug = (function () {
-    var DEBUGGING = false,
+    var DEBUGGING = true,
         debugCanvas;
     
     function isOn() {
         return DEBUGGING;
     }
     
-    function proxyEvents(source, dest) {
-        for (var i = 2; i < arguments.length; i++) {
-            source.addEventListener(arguments[i], function (e) {
-                dest.dispatchEvent(e);
-            },false); 
-        }
-    }
-    
     function run(fn) {
         if (DEBUGGING) {
-            console.log('debig.run', fn);
             fn();
         }
     }
@@ -40,16 +31,12 @@ puckt.debug = (function () {
             debugDraw.SetSprite(stage.canvas.getContext("2d"));
             debugDraw.SetFlags(box2d.b2DebugDraw.e_shapeBit | box2d.b2DebugDraw.e_jointBit);
             world.SetDebugDraw(debugDraw);
-            
-            // Set up event proxies
-            //puckt.debug.proxyEvents(debugCanvas, stage.canvas, 'mousedown', 'mousemove', 'mouseup');
         }
     }
     
     return {
         init: init,
         isOn: isOn,
-        proxyEvents: proxyEvents,
         run: run,
         canvas: debugCanvas
     }
