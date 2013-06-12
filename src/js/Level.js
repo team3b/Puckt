352b1308527f173556c9d88a3161245f9d069e52
Joshua Croad <josh.croad@gmail.com>
2013-06-12 13:47:34 +0100

    Began design Level class to handle different levels

--- src/js/Level.js
+++ src/js/Level.js
@@ -2,8 +2,33 @@
 
 var puckt = puckt || {};
 puckt.Level = (function () {
-    function Level() {
-        
+    var lvl;
+    function Level (number) {
+        // Level constructor
+        get(number);
+    }
+
+    function get (number) {
+        // AJAX call to retreive level definition
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", 'levels/'+number+'.json', true);
+        xhr.responseType = 'text';
+        // Once ready state is 4
+        xhr.onload = function (e) {
+            if (this.status == 200) {
+                lvl = JSON.parse(xhr.responseText);
+                console.log(lvl);
+            }
+        }
+        xhr.send(null);
+    }
+
+    function drawBoundaries () {
+        // Draw the boundaries
+    }
+
+    function draw () {
+        // Draw the object
     }
     
     return Level;
