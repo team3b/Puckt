4e5a9f119189c59c0ff17a1b6dee51936d85140b
James Atkin <04jatkin@gmail.com>
2013-06-14 09:51:57 +0100

    Temporarily removed music

--- src/js/music.js
+++ src/js/music.js
@@ -5,22 +5,22 @@ puckt.music = (function () {
 	var sounds = {}, counter = 0, music,
 
 	load = function (readyCallback) {
-		createjs.Sound.addEventListener("fileload", loadHandler);
-		createjs.Sound.registerSound("audio/background.mp3", "backgroundMusic", 1);
+		// createjs.Sound.addEventListener("fileload", loadHandler);
+		// createjs.Sound.registerSound("audio/background.mp3", "backgroundMusic", 1);
 
-		function loadHandler (event) {
-			music = createjs.Sound.createInstance(event.id);
-			readyCallback();
-		}
+		// function loadHandler (event) {
+		// 	music = createjs.Sound.createInstance(event.id);
+		// 	readyCallback();
+		// }
 
 	},
 
 	play = function () {
-		music.play();
+		// music.play();
 	},
 
 	setVolume = function (vol) {
-		music.setVolume(vol);
+		// music.setVolume(vol);
 	};
 
 	return {
