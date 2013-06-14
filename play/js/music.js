b8dc5f10fd0c80769a013dff0dc6c9299ec7db1b
James Atkin <04jatkin@gmail.com>
2013-06-14 12:06:47 +0100

    Committing more table version (no music)

--- play/js/music.js
+++ play/js/music.js
@@ -2,36 +2,49 @@
 
 var puckt = puckt || {};
 puckt.music = (function () {
-	var sounds = {}, counter = 0, music, loader,
+	var sounds = {}, counter = 0, music, loader, volume = 1, playing = false,
 
 	load = function (readyCallback) {
-		loader = new createjs.LoadQueue();
-		loader.onComplete = loadHandler;
-		loader.installPlugin(createjs.Sound);
-		loader.loadFile({id: 'bg', src:'audio/background.mp3'});
-		// createjs.Sound.addEventListener("fileload", loadHandler);
-		// createjs.Sound.registerSound("audio/background.mp3", "backgroundMusic", 1);
+		// loader = new createjs.LoadQueue();
+		// loader.onComplete = loadHandler;
+		// loader.installPlugin(createjs.Sound);
+		// loader.loadFile({id: 'bg', src:'audio/background.mp3'});
+		// // createjs.Sound.addEventListener("fileload", loadHandler);
+		// // createjs.Sound.registerSound("audio/background.mp3", "backgroundMusic", 1);
 
-		function loadHandler (event) {
-			console.log('loadHandler', event, music);
-			music = createjs.Sound.createInstance('bg');
-			console.log('loadHandler', event, music);
-			music.play();
-		}
+		// function loadHandler (event) {
+		// 	console.log('loadHandler', event, music);
+		// 	music = createjs.Sound.createInstance('bg');
+		// 	console.log('loadHandler', event, music);
+		// 	music.setVolume(volume);
+		// 	music.play();
+		// }
 
 	},
 
 	play = function () {
-		// music.play();
+		// if (music != null) {
+		// 	music.play();
+		// }
+	},
+
+	stop = function () {
+		// if (music != null) {
+		// 	music.stop();
+		// }
 	},
 
 	setVolume = function (vol) {
-		// music.setVolume(vol);
+		// volume = vol;
+		// if (music != null) {
+		// 	music.setVolume(volume);
+		// }
 	};
 
 	return {
 		load: load,
 		play: play,
+		stop: stop,
 		setVolume: setVolume
 	};
 })();
