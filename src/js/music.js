dd4441256d1d761545137c22c12e84e218527451
James Atkin <04jatkin@gmail.com>
2013-06-14 11:41:52 +0100

    Added music

--- src/js/music.js
+++ src/js/music.js
@@ -2,16 +2,22 @@
 
 var puckt = puckt || {};
 puckt.music = (function () {
-	var sounds = {}, counter = 0, music,
+	var sounds = {}, counter = 0, music, loader,
 
 	load = function (readyCallback) {
+		loader = new createjs.LoadQueue();
+		loader.onComplete = loadHandler;
+		loader.installPlugin(createjs.Sound);
+		loader.loadFile({id: 'bg', src:'audio/background.mp3'});
 		// createjs.Sound.addEventListener("fileload", loadHandler);
 		// createjs.Sound.registerSound("audio/background.mp3", "backgroundMusic", 1);
 
-		// function loadHandler (event) {
-		// 	music = createjs.Sound.createInstance(event.id);
-		// 	readyCallback();
-		// }
+		function loadHandler (event) {
+			console.log('loadHandler', event, music);
+			music = createjs.Sound.createInstance('bg');
+			console.log('loadHandler', event, music);
+			music.play();
+		}
 
 	},
 
