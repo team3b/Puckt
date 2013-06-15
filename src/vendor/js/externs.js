5e2a86aaa8e96e1c3ba009e45d797ad9b451bdda
James Atkin <04jatkin@gmail.com>
2013-06-15 17:38:01 +0100

    Compiled version of the game with music enabled

---
+++ src/vendor/js/externs.js
@@ -0,0 +1,21 @@
+// Externs for Class.js
+function Class() {};
+
+// Externs for 
+var createjs = {
+
+};
+
+// Externs for Box2dWeb-1.1.a.3.js
+var Box2D = {};
+
+Box2D.Common.Math.b2Vec2 = function () {};
+Box2D.Dynamics.b2BodyDef = function () {};
+Box2D.Dynamics.b2Body = function () {};
+Box2D.Dynamics.b2FixtureDef = function () {};
+Box2D.Dynamics.b2Fixture = function () {};
+Box2D.Dynamics.b2World = function () {};
+Box2D.Collision.Shapes.b2MassData = function () {};
+Box2D.Collision.Shapes.b2PolygonShape = function () {};
+Box2D.Collision.Shapes.b2CircleShape = function () {};
+Box2D.Dynamics.b2DebugDraw = function () {};
