c2882b5bd0b52e0c6aba3c99a417df6287f9d1f9
James Atkin <04jatkin@gmail.com>
2013-06-15 17:41:40 +0100

    Disabled music again, as it crashes my iPod touch

--- src/js/music.js
+++ src/js/music.js
@@ -5,37 +5,38 @@ puckt.music = (function () {
 	var sounds = {}, counter = 0, music, loader, volume = 1, playing = false,
 
 	load = function (readyCallback) {
-		loader = new createjs.LoadQueue();
-		loader.onComplete = loadHandler;
-		loader.installPlugin(createjs.Sound);
-		loader.loadFile({id: 'bg', src:'audio/background.mp3'});
+		// loader = new createjs.LoadQueue();
+		// loader.onComplete = loadHandler;
+		// loader.installPlugin(createjs.Sound);
+		// loader.loadFile({id: 'bg', src:'audio/background.mp3'});
 
-		function loadHandler (event) {
-			console.log('loadHandler', event, music);
-			music = createjs.Sound.createInstance('bg');
-			console.log('loadHandler', event, music);
-			music.setVolume(volume);
-			music.play();
-		}
+		// function loadHandler (event) {
+		// 	console.log('loadHandler', event, music);
+		// 	music = createjs.Sound.createInstance('bg');
+		// 	console.log('loadHandler', event, music);
+		// 	music.setVolume(volume);
+		// 	music.play();
+		// }
+
 	},
 
 	play = function () {
-		if (music != null) {
-			music.play();
-		}
+		// if (music != null) {
+		// 	music.play();
+		// }
 	},
 
 	stop = function () {
-		if (music != null) {
-			music.stop();
-		}
+		// if (music != null) {
+		// 	music.stop();
+		// }
 	},
 
 	setVolume = function (vol) {
-		volume = vol;
-		if (music != null) {
-			music.setVolume(volume);
-		}
+		// volume = vol;
+		// if (music != null) {
+		// 	music.setVolume(volume);
+		// }
 	};
 
 	return {
